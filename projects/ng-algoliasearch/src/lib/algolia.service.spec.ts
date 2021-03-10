712be90900d98a9d3da4db9c930484537387adb2
Kimura Yu <33382781+KimuraYu45z@users.noreply.github.com>
2021-03-10 12:15:23 +0900

    feat: test

---
+++ projects/ng-algoliasearch/src/lib/algolia.service.spec.ts
@@ -0,0 +1,6 @@
+// import { ComponentFixture, TestBed } from '@angular/core/testing';
+// import { AlgoliaService } from './algolia.service';
+
+describe('AlgoliaService', () => {
+  expect(true).toBeTruthy();
+});
